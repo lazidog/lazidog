f78cab28983ae5237e34857fef82ef6aa9ad357b
lazidog <nvtien247@gmail.com>
2024-07-24 15:23:32 +0700

    Revert "tic tac boom"
    This reverts commit f4b438a66df279edce792641c6ba3e0b0663f785.

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+import './globals.css';
+import { Rubik } from 'next/font/google';
+
+const font = Rubik({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Lazidog',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
