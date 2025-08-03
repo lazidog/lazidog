e5606929ad87d5d4472fef550cd12451e008239d
lazidog <nvtien247@gmail.com>
2025-08-03 10:00:06 +0700

    rm packages/db

--- packages/db/prisma/migrations/20240514102813_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "users" (
-    "id" UUID NOT NULL,
-    "email" TEXT NOT NULL,
-    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
--- packages/db/prisma/migrations/20240515015345_auto_gen_user_id/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
